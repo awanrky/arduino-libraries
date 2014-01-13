7fb0e4f8b6d48d0e22d5cec146926c30223081df
Mark Ott <mark.ott@live.com>
2014-01-12 23:50:08 -0600

    updated BMP180 to have more consistent function names

--- aisa_BMP180/aisa_BMP180.cpp
+++ aisa_BMP180/aisa_BMP180.cpp
@@ -3,6 +3,7 @@
 aisa_BMP180::aisa_BMP180(String name)
 {
 	sensorName = name;
+	clearErrorState();
 	bmp = new Adafruit_BMP085_Unified(10085);
 
 	isInitialized = false;
@@ -12,26 +13,33 @@ aisa_BMP180::aisa_BMP180(String name)
 	}
 }
 
+String aisa_BMP180::getSensorName()
+{
+	return sensorName;
+}
 
-float aisa_BMP180::getTemperature()
+float aisa_BMP180::getDegreesCelcius()
 {
 	float temperature;
 
 	if (!isInitialized) 
 	{
+		setErrorStateSensorNotFound();
 		return -999.999;
 	}
 	
 	bmp->getTemperature(&temperature);
+	clearErrorState();
 	return temperature;
 }
 
-float aisa_BMP180::getBarometricPressure()
+float aisa_BMP180::getHectoPascals()
 {
 	sensors_event_t event;
 
 	if (!isInitialized)
 	{
+		setErrorStateSensorNotFound();
 		return 0.0;
 	}
 
@@ -39,41 +47,63 @@ float aisa_BMP180::getBarometricPressure()
 
 	if (event.pressure)
 	{
+		clearErrorState();
 		return event.pressure;
 	}
+	setErrorStateSensorError();
 	return 0.0;
 }
 
-float aisa_BMP180::getAltitude(float seaLevelPressure)
+float aisa_BMP180::getAltitudeInMeters(float seaLevelPressure)
 {
 	float pressure;
 	float temperature;
 
-	pressure = this->getBarometricPressure();
+	if (!isInitialized)
+	{
+		setErrorStateSensorNotFound();
+		return 0.0;
+	}
+
+	pressure = this->getHectoPascals();
 	if (pressure == 0)
 	{
+		setErrorStateSensorError();
 		return -999.999;
 	}
 
-	temperature = this->getTemperature();
+	temperature = this->getDegreesCelcius();
 	if (temperature < -999)
 	{
+		setErrorStateSensorError();
 		return -999.999;
 	}
 
+	clearErrorState();
 	return bmp->pressureToAltitude(seaLevelPressure, pressure, temperature);
 }
 
-float aisa_BMP180::getAltitude()
+float aisa_BMP180::getAltitudeInMeters()
 {
-	return this->getAltitude(SENSORS_PRESSURE_SEALEVELHPA);
+	return this->getAltitudeInMeters(SENSORS_PRESSURE_SEALEVELHPA);
 }
 
-String aisa_BMP180::getErrorState()
+int aisa_BMP180::getErrorState()
 {
-	if (isInitialized)
-	{
-		return "Sensor Error";
-	}
-	return "Sensor Not Found";
+	return errorState;
+}
+
+void aisa_BMP180::setErrorStateSensorError()
+{
+	errorState = AISA_BMP180_ERROR_STATE_SENSOR_ERROR;
+}
+
+void aisa_BMP180::setErrorStateSensorNotFound()
+{
+	errorState = AISA_BMP180_ERROR_STATE_SENSOR_NOT_FOUND;
+}
+
+void aisa_BMP180::clearErrorState()
+{
+	errorState = AISA_BMP180_ERROR_STATE_NONE;
 }
--- aisa_BMP180/aisa_BMP180.h
+++ aisa_BMP180/aisa_BMP180.h
@@ -6,24 +6,38 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP085.h>
 
+enum
+{
+	AISA_BMP180_ERROR_STATE_NONE				= 0,
+	AISA_BMP180_ERROR_STATE_SENSOR_NOT_FOUND 	= 1,
+	AISA_BMP180_ERROR_STATE_SENSOR_ERROR		= 2
+};
+
 class aisa_BMP180
 {
 private:
 	bool isInitialized;
 
 	String sensorName;
+	int errorState;
 
 	Adafruit_BMP085_Unified * bmp;
+
+	void setErrorStateSensorError();
+	void setErrorStateSensorNotFound();
+	void clearErrorState();
 
 public:
 	aisa_BMP180(String name);
 
-	float getTemperature();
-	float getBarometricPressure();
-	float getAltitude();
-	float getAltitude(float seaLevelPressure);
+	String getSensorName();
 
-	String getErrorState();
+	float getDegreesCelcius();
+	float getHectoPascals();
+	float getAltitudeInMeters();
+	float getAltitudeInMeters(float seaLevelPressure);
+
+	int getErrorState();
 
 };
 
